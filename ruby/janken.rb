12d89ec60cadd30c21ddbbb73be6505e99c9ef2a
Shuichi Fujita <chaashuuu@gmail.com>
2017-02-24 22:09:18 +0900

    判定ロジックの変更
    まだ途中だよ

--- ruby/janken.rb
+++ ruby/janken.rb
@@ -7,38 +7,26 @@
 # 勝ち負け判定
 # 続ける？やめる？
 # break
+
+# 数値でやってみよう
 # **** end **** #
 
 puts "じゃんけんをしましょう"
 
 # functions
-def isValid(input, hash)
-  !input.empty? && input.kind_of?(String) && !!input.match(/[A-C]/)
-end
-
-def updateAttribute(you, janken)
-  if you == janken[:A]
-    {:strong => janken[:B], :draw => janken[:A], :weak => janken[:C]}
-  elsif you == janken[:B]
-    {:strong => janken[:C], :draw => janken[:B], :weak => janken[:A]}
-  elsif you == janken[:C]
-    {:strong => janken[:A], :draw => janken[:C], :weak => janken[:B]}
+def isValid(input, janken)
+  if !input.empty? && input.kind_of?(String)
+    janken.each do |key, val|
+      !!input.match(key.to_s)
+    end
   end
 end
 
-def judge(enemy, attribute)
-  state = ""
-  if enemy == attribute[:strong]
-    state = "Win."
-  elsif enemy == attribute[:draw]
-    state = "Draw."
-  elsif enemy == attribute[:weak]
-    state = "Lose"
-  end
-  state
+def judge(you, enemy)
+  you + enemy # janken.rb:26:in `judge': undefined method `+' for nil:NilClass (NoMethodError)
 end
 
-def shouldBreak()
+def shouldpaperreak()
   state = gets.chomp!
   if state == "y" || state == "n"
     return state == "n"
@@ -49,17 +37,12 @@ def shouldBreak()
 end
 
 # initialize
-janken = { A: :ぐー, B: :ちょき, C: :ぱー }
+janken = { rock: 1, paper: 2, scissors: 3 }
 messages = {
-  :intro => "なにだすの？\nA: #{janken[:A]}\nB: #{janken[:B]}\nC: #{janken[:C]}",
+  :intro => "なにだすの？\nrock: #{janken[:rock]}\npaper: #{janken[:paper]}\nscissors: #{janken[:scissors]}",
   :error => "有効な値ではない。",
   :quit => "終了します。",
   :continue => "まだ続けるか？: y or n"
-}
-attribute = {
-  :strong => "",
-  :draw => "",
-  :weak => ""
 }
 result = ""
 continue = false
@@ -87,8 +70,7 @@ loop do
     puts "You: #{you} Enemy: #{enemy}"
 
     # judgment
-    attribute = updateAttribute(you, janken)
-    result = judge(enemy, attribute)
+    result = judge(you, enemy)
     puts "Result: #{result}"
     puts messages[:continue]
 
